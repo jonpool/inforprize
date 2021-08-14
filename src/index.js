2fc91a48a9fdbd65ce3961fe8ea8ec5a76182024
jonpool <joaopl_4@hotmail.com>
2021-08-14 11:14:40 -0400

    homeUpdate

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.scss';
 import PageScrollTop from './component/PageScrollTop';
 
 // Home layout
-import Demo from './page-demo/Demo';
+//import Demo from './page-demo/Demo';
 import MainDemo from './home/MainDemo';
 import Startup from './home/Startup';
 import Paralax from './home/Paralax';
@@ -67,7 +67,7 @@ class Root extends Component{
             <BrowserRouter basename={'/'}>
                 <PageScrollTop>
                     <Switch>
-                        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Demo}/>
+                        <Route exact path={`${process.env.PUBLIC_URL}/`} component={HomeParticles}/>
                         <Route exact path={`${process.env.PUBLIC_URL}/main-demo`} component={MainDemo}/>
                         <Route exact path={`${process.env.PUBLIC_URL}/dark-main-demo`} component={DarkMainDemo}/>
                         <Route exact path={`${process.env.PUBLIC_URL}/startup`} component={Startup}/>
